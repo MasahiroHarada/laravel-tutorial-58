43f929d33c615650c6d0062b04aaede40d5a97ad
Masahiro Harada <poe.422@gmail.com>
2019-08-03 15:31:17 +0000

    認証機能

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+Route::get('/', 'HomeController@index')->name('home');
+
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
@@ -9,3 +11,5 @@ Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+
+Auth::routes();
